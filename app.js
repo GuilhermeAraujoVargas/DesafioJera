9b8ac8349f7a5deff9fca43ee0065c7cc23d9dc7
GuilhermeAraujoVargas <guilhermearavargas@gmail.com>
2020-12-04 11:09:40 -0400

    Atualização 3 - Criação da função de verificação do status de pomodoro e descanso

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ let inputSegundos = document.querySelector(".segundos");
 
 let btnIniciar= document.querySelector(".btnIniciar");
 
+//variável para controle e contagem do pomodoro/descanso
+let contadorPomodoro = 1;
+
+function verificarStatus() {
+    ++contadorPomodoro;
+    if (inputMinutos.value = 0 && inputSegundos.value == 0 && contadorPomodoro % 2 == 0) {
+        inputMinutos.value = 5;
+        inputMinutos.innerHTML = inputMinutos.value;
+        inputSegundos.innerHTML = inputSegundos.value;
+    } else if (inputMinutos.value == 0 && inputSegundos.value == 0 && contadorPomodoro % 2 != 0) {
+        inputMinutos.value = 25;
+        inputMinutos.innerHTML = inputMinutos.value;
+        inputSegundos.innerHTML = inputSegundos.value;
+    }
+}
+
 //função para realizar o cronômetro regressivo
 function temporizador() {
     if (inputMinutos.value > 0 && inputSegundos.value == 0) {
