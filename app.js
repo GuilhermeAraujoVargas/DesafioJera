d33d6b727515d78599dd726890a0aee492d12efb
GuilhermeAraujoVargas <guilhermearavargas@gmail.com>
2020-12-04 10:58:03 -0400

    Atualização 2 - Criação da função responsável pelo cronômetro

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ let inputSegundos = document.querySelector(".segundos");
 
 let btnIniciar= document.querySelector(".btnIniciar");
 
+//função para realizar o cronômetro regressivo
+function temporizador() {
+    if (inputMinutos.value > 0 && inputSegundos.value == 0) {
+        inputMinutos.value--;
+        inputSegundos.value = 59;
+        inputMinutos.innerHTML = inputMinutos.value;
+        inputSegundos.innerHTML = inputSegundos.value;
+    } else if (inputMinutos.value >= 0 && inputSegundos.value > 0) {
+        inputSegundos.value--;
+        inputSegundos.innerHTML = inputSegundos.value;  
+    } else if (inputMinutos.value == 0 && inputSegundos.value == 0) {
+        verificarStatus();
+    }
+}
+
+
+//botão para iniciar o temporizador/descanso
 btnIniciar.addEventListener("click", () => {
     temporizador();
 })
