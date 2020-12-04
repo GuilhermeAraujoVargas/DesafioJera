cea06fead35bc55b7329f9059ea83e4274ac4b62
GuilhermeAraujoVargas <guilhermearavargas@gmail.com>
2020-12-04 10:49:26 -0400

    Atualização 1 - Criação do evento do botão

---
+++ app.js
@@ -0,0 +1,8 @@
+let inputMinutos = document.querySelector(".minutos");
+let inputSegundos = document.querySelector(".segundos");
+
+let btnIniciar= document.querySelector(".btnIniciar");
+
+btnIniciar.addEventListener("click", () => {
+    temporizador();
+})
